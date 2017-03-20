e07f9c9a05d6a396c19a831b553abdaebde9cb4b
sakmor <sakmor@gmail.com>
2017-03-20 18:48:49 +0800

    開始專案

---
+++ Assets/main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
